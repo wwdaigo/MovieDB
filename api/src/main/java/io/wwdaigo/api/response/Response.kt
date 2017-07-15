2d78090ff4ddd4a2603e29aecb52847463235064
Daigo Matsuoka <marcelo.daigo@gmail.com>
2017-07-15 20:27:30 -0300

    New fields in Response data class

--- api/src/main/java/io/wwdaigo/api/response/Response.kt
+++ api/src/main/java/io/wwdaigo/api/response/Response.kt
@@ -1,9 +1,16 @@
 package io.wwdaigo.api.response
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * Created by daigomatsuoka on 15/07/17.
  */
 data class Response<T>(
         val id: Int,
+        val page: Int,
+        @SerializedName("total_pages")
+        val totalPages: Int,
+        @SerializedName("total_results")
+        val totalResults: Int,
         val results: List<T>
 )
