9c032ef24e4128c45db76ba9af31a9e640330649
Daigo Matsuoka <marcelo.daigo@gmail.com>
2017-07-15 20:20:58 -0300

    Review Data object

---
+++ data/src/main/java/io/wwdaigo/data/ReviewData.kt
@@ -0,0 +1,12 @@
+package io.wwdaigo.data
+
+/**
+ * Created by daigomatsuoka on 15/07/17.
+ */
+
+data class ReviewData (
+        val id: String,
+        val author: String,
+        val content: String,
+        val url: String
+)
