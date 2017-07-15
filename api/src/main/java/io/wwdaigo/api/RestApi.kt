224c01054b05b71110204d1da0bd8a0f88ab102e
Daigo Matsuoka <marcelo.daigo@gmail.com>
2017-07-15 19:57:35 -0300

    Setup Retrofit RestAPI caller

---
+++ api/src/main/java/io/wwdaigo/api/RestApi.kt
@@ -0,0 +1,18 @@
+package io.wwdaigo.api
+
+import io.wwdaigo.api.commons.BASE_URL
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+/**
+ * Created by daigomatsuoka on 15/07/17.
+ */
+class RestApi {
+
+    val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+}
---
+++ api/src/main/java/io/wwdaigo/api/commons/Constants.kt
@@ -0,0 +1,8 @@
+package io.wwdaigo.api.commons
+
+/**
+ * Created by daigomatsuoka on 15/07/17.
+ */
+
+const val BASE_URL = "https://api.themoviedb.org/3/"
+const val API_KEY = "714c25cc9c6e20eb5fcffdd15fbc4397"
