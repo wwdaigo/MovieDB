9ec67860707d8f8d7e85ace1c91a0f8ad5ddac44
Daigo Matsuoka <marcelo.daigo@gmail.com>
2017-07-15 20:22:53 -0300

    Video Data Object

---
+++ data/src/main/java/io/wwdaigo/data/VideoData.kt
@@ -0,0 +1,13 @@
+package io.wwdaigo.data
+
+/**
+ * Created by daigomatsuoka on 15/07/17.
+ */
+data class VideoData(
+        val id: String,
+        val key: String,
+        val name: String,
+        val site: String,
+        val size: Int,
+        val type: String
+)
