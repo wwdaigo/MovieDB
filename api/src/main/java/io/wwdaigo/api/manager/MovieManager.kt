d6c40ca83e63311396a0a7ac0f95aec95047612f
Daigo Matsuoka <marcelo.daigo@gmail.com>
2017-07-16 02:17:29 -0300

    Implements the manager api layer

--- api/src/main/java/io/wwdaigo/api/manager/MovieManager.kt
+++ api/src/main/java/io/wwdaigo/api/manager/MovieManager.kt
@@ -5,6 +5,8 @@ import io.wwdaigo.api.RestApi
 import io.wwdaigo.api.commons.API_KEY
 import io.wwdaigo.api.response.Response
 import io.wwdaigo.data.MovieData
+import io.wwdaigo.data.ReviewData
+import io.wwdaigo.data.VideoData
 
 /**
  * Created by daigomatsuoka on 15/07/17.
@@ -13,5 +15,13 @@ import io.wwdaigo.data.MovieData
 class MovieManager {
     fun getPopular():Observable<Response<MovieData>> {
         return RestApi().movieRequest.getMoviePopular(API_KEY)
+    }
+
+    fun getVideos(movieId: Int): Observable<Response<VideoData>> {
+        return RestApi().movieRequest.getMovieVideos(API_KEY, movieId)
+    }
+
+    fun getReviews(movieId: Int): Observable<Response<ReviewData>> {
+        return RestApi().movieRequest.getMovieReviews(API_KEY, movieId)
     }
 }
