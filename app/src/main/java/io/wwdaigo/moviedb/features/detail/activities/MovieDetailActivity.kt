832d4337eb4a3f1f4c0148937fa92b426167dcfd
Daigo Matsuoka <marcelo.daigo@gmail.com>
2017-07-16 01:45:34 -0300

    Create a new activity for Detail

---
+++ app/src/main/java/io/wwdaigo/moviedb/features/detail/activities/MovieDetailActivity.kt
@@ -0,0 +1,14 @@
+package io.wwdaigo.moviedb.features.detail.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import io.wwdaigo.moviedb.R
+
+class MovieDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_detail)
+    }
+}
