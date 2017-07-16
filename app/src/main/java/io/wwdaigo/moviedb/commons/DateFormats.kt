f3b950e5136520777a8e598c65c3da11a395b5b8
Daigo Matsuoka <marcelo.daigo@gmail.com>
2017-07-16 14:45:47 -0300

    Create a date formatter

---
+++ app/src/main/java/io/wwdaigo/moviedb/commons/DateFormats.kt
@@ -0,0 +1,10 @@
+package io.wwdaigo.moviedb.commons
+
+/**
+ * Created by daigomatsuoka on 16/07/17.
+ */
+
+enum class DateFormats (val format: String) {
+    YYYY_MM_DD("yyyy-MM-dd"),
+    YEAR("yyyy")
+}
---
+++ app/src/main/java/io/wwdaigo/moviedb/commons/StringExt.kt
@@ -0,0 +1,9 @@
+package io.wwdaigo.moviedb.commons
+
+/**
+ * Created by daigomatsuoka on 16/07/17.
+ */
+
+fun String.formatDate(from: DateFormats, to: DateFormats): String {
+    TODO("Implement")
+}
