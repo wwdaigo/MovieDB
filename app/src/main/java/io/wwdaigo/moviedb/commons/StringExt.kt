f788daaf3c45e31e69e40eb2bc04d8c5e1b26e3d
Daigo Matsuoka <marcelo.daigo@gmail.com>
2017-07-16 14:50:13 -0300

    Implements date formatter

--- app/src/main/java/io/wwdaigo/moviedb/commons/StringExt.kt
+++ app/src/main/java/io/wwdaigo/moviedb/commons/StringExt.kt
@@ -1,9 +1,15 @@
 package io.wwdaigo.moviedb.commons
 
+import java.text.SimpleDateFormat
+import java.util.*
+
 /**
  * Created by daigomatsuoka on 16/07/17.
  */
 
 fun String.formatDate(from: DateFormats, to: DateFormats): String {
-    TODO("Implement")
+    val simpleDateFormatFrom = SimpleDateFormat(from.format, Locale.getDefault())
+    val simpleDateFormatTo = SimpleDateFormat(to.format, Locale.getDefault())
+    val parsedDate = simpleDateFormatFrom.parse(this)
+    return simpleDateFormatTo.format(parsedDate)
 }
